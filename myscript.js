4c187bde1e9383a96db98f5401e7954de107aa46
Kseniia <xeniiatikhomirova@gmail.com>
2024-03-27 21:10:57 +0400

    updated repeatative parts

--- myscript.js
+++ myscript.js
@@ -1,45 +1,29 @@
-const rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"];
+function makeRows(sectionLegth, rowLength, placement) {
+   const rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"];
+   let html = "";
+   let counter = 1;
 
-let html = "";
-let counter = 1;
-
-rows.forEach(row => {
-   html += `<div class="label">${row}</div>`;
-   
-   for(let i=0; i < 3; i++) {
-      html += `<div id="${row + counter}">${counter}</div>`;
-      counter++;
-   }
-   counter = counter + 12;
-});
-document.getElementById("left").innerHTML = html;
-
-html = "";
-counter = 1;
-
-rows.forEach(row => {
-   counter = counter + 12;
-   for(let i=0; i < 3; i++) {
-      html += `<div id="${row + counter}">${counter}</div>`;
-      counter++;
-   }
-   html += `<div class="label">${row}</div>`;
-});
-
-document.getElementById("right").innerHTML = html;
-
-html = "";
-counter = 1;
-
-rows.forEach(row => {
-   counter = counter + 3;
-   for(let i=0; i < 9; i++) {
-      html += `<div id="${row + counter}">${counter}</div>`;
-      counter++;
-   }
-   counter = counter + 3;
-});
-document.getElementById("middle").innerHTML = html;
+   rows.forEach( row => {
+      switch(placement) {
+         case "right": counter = counter + (rowLength - sectionLegth) ; break;
+         case "left": html += `<div class="label">${row}</div>`; break;
+         default: counter = counter + ((rowLength - sectionLegth) / 2); break;
+      }
+      for (let i=0; i < sectionLegth; i++) {
+         html += `<div class="a" id="${row + counter}">${counter}</div>`;
+         counter++;
+      }
+      switch(placement) {
+         case "right": html += `<div class="label">${row}</div>`; break;
+         case "left": counter = counter + (rowLength - sectionLegth) ; break;
+         default: counter = counter + ((rowLength - sectionLegth) / 2); break;
+      }
+   });
+   document.getElementById(placement).innerHTML = html;
+}
+makeRows(3, 15, "left");
+makeRows(3, 15, "right");
+makeRows(9, 15, "middle");
 
 
 
